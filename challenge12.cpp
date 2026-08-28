c4102dda193e44b1d499a7bd3fe466475ac53197
Spencer Thompson <spencer.thompson1@students.snow.edu>
2026-08-28 16:55:07 -0600

    add challenge12

---
+++ challenge12.cpp
@@ -0,0 +1,19 @@
+// Challenge 12
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //intializing variables
+    int acre = 43560,
+        totalSqFt = 391876,
+        numAcres;
+
+    // Calculation
+    numAcres = totalSqFt/acre;
+    //output
+    cout << "Your " << totalSqFt << " SqFt is " << numAcres << "acres";
+
+    return 0;
+}
